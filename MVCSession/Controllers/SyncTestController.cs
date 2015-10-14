1b62178efee8a0b4ee17a8b031ac26856c9f9d99
jlw123199 <jlw123199@hotmail.com>
2015-10-14 22:24:19 +0800

    Add Sync funtion

---
+++ MVCSession/Controllers/SyncTestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSession.Controllers
+{
+    public class SyncTestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [OutputCache(NoStore = true, Duration = 0)]
+        public string GetMessage(int id)
+        {
+            return string.Format("Message: {0}", id);
+        }
+    }
+}
